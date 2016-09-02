681f1bf2c1a64649b38c74c0568579a276112cc3
gff1055 <gff105@gmail.com>
2016-09-01 23:34:11 -0300

    Implementacao de um controller no arquivo app.js. Modificacao estrutural em uma das paginas de listagem e no index

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var app = angular.module('app',[ngRoute]);
+var app = angular.module('app',['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 	when('/house', {controller: 'houseController', templateUrl: 'pageListDjsHouse.html'}).
-	otherwise({redirectTo: '/house'});
+	otherwise({redirectTo: '/'});
 }]);
+
+app.controller('houseController', function($scope, $http){
+	$scope.djsHouse = Array();
+	$scope.listingDjsHouse = function(){
+		$http.get("listDjsHouse.html").success(function($data){
+			$scope.djsHouse = data.djsHouse;
+			console.log($scope.djsHouse);
+		}).error(function($data){
+			alert("ERRO...");
+			console.log($data);
+		});
+			
+	}
+});
 
 
