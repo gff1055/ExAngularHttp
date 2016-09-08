2d74310906ce460629acbfd2b85f4d3743f07947
gff1055 <gff105@gmail.com>
2016-09-08 00:03:08 -0300

    Efetuado ultimos ajustes em uma das  paginas de listagem

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-/*Criacao de modulo cujo nome é 'app' e uma referencia ao modulo ngRoute que é usado para criar
-as rotas*/
+//Criacao de modulo cujo nome é 'app'
+//Criacao de uma referencia ao modulo ngRoute que é usado para criar as rotas
 var app = angular.module('app',['ngRoute']);
 
 /*configurando o modulo. Nesse caso esta sendo configurada a funcionalidade Router
@@ -37,9 +37,16 @@ app.controller('houseController', function($scope, $http){
 		//success – Executa se a requisicao GET for realizada com sucesso
 		//variavel dado – a resposta do servidor
 		success(function(dado){
+			
+			//Aqui dado recebe a "massa" de dados, sem qualquer tratamento.
+			//Esse tratamento será feito na view
 			$scope.djsHouse = dado.djsHouse;
+			
 			console.log($scope.djsHouse);
-		}).error(function(dado){
+		})
+		
+		//executa no caso de erro
+		.error(function(dado){
 			alert("ERRO...");
 			console.log(dado);
 		});
@@ -48,9 +55,32 @@ app.controller('houseController', function($scope, $http){
 });
 
 app.controller('tranceController', function($scope, $http){
+	$scope.djsTrance = Array();
 	
-
-}
-)
+	//associando variavel a uma funcao
+	$scope.listingDjsTrance = function(){
+		
+		//por meio do $http.get realiza-se uma requisicao Ajax acessando o arquivo que contem os
+		//os dados necessarios para a resposta JSON*/
+		$http.get("listDjsTrance.html").
+		
+		//Executa se a requisicao get for realizada com sucesso.
+		//Dado se refere a resposta do servidor
+		success(function(dado){
+			
+			//Aqui dado recebe a "massa" de dados, sem qualquer tratamento.
+			//Esse tratamento será feito na view
+			$scope.djsTrance = dado.djsTrance;
+			
+			console.log($scope.djsTrance);
+		}).
+		
+		//Executa se a requisicao nao for realizada com sucesso
+		error(function(dado){
+			alert("ERRO...");
+			console.log(dado);
+		});
+	}
+});
 
 
